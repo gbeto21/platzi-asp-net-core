09ce5c64b14569cb0c25cf875b2223260180e56e
Alberto González Balderas <albertoglezb@hotmail.com>
2019-10-12 19:18:57 -0500

    Se crearon las clases modelos y se modificó la vista que muestra la escuela.

--- Controllers/EscuelaController.cs
+++ Controllers/EscuelaController.cs
@@ -9,8 +9,8 @@ namespace platzi_asp_net_core.Controllers
         public IActionResult Index()
         {
             var escuela = new Escuela();
-            escuela.Year = 2005;
-            escuela.EscuelaId = Guid.NewGuid().ToString();
+            escuela.AñoDeCreación = 2005;
+            escuela.UniqueId = Guid.NewGuid().ToString();
             escuela.Nombre = "Platzi Scool";
 
             ViewBag.CosaDinamica ="La monja.";
