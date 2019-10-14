d8cb4e6adf437332698195c6cdef7fb97f72c924
Alberto González Balderas <albertoglezb@hotmail.com>
2019-10-14 13:03:17 -0500

    Se creó la vista parcial de una asignatura y se creó el método para mostrar varias asignaturas.

--- Controllers/AsignaturaController.cs
+++ Controllers/AsignaturaController.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using platzi_asp_net_core.Models;
 
@@ -12,11 +13,25 @@ namespace platzi_asp_net_core.Controllers
             var asignatura = new Asignatura()
             {
                 UniqueId = Guid.NewGuid().ToString(),
-                Nombre = "Programación"
+                Nombre = "Programación Orientada a Objetos."
             };
 
             ViewBag.Fecha = DateTime.Now;
             return View(asignatura);
+        }
+
+        public IActionResult MultiAsignatura()
+        {
+
+            var asignaturas = new List<Asignatura>(){
+                            new Asignatura{Nombre="Matemáticas", UniqueId = Guid.NewGuid().ToString()} ,
+                            new Asignatura{Nombre="Educación Física", UniqueId = Guid.NewGuid().ToString()},
+                            new Asignatura{Nombre="Castellano", UniqueId = Guid.NewGuid().ToString()},
+                            new Asignatura{Nombre="Ciencias Naturales", UniqueId = Guid.NewGuid().ToString()},
+                            new Asignatura{Nombre="Programación OO", UniqueId = Guid.NewGuid().ToString()}
+                };
+
+            return View(asignaturas);
         }
 
     }
