5bbfc75619d21c45de1a4e036805bdb090fb68a7
Alberto González Balderas <albertoglezb@hotmail.com>
2019-10-18 16:21:56 -0500

    Se restrucutró la siembra de datos, con los cambios realizados a los modelos.

--- Models/EscuelaContext.cs
+++ Models/EscuelaContext.cs
@@ -29,35 +29,73 @@ namespace platzi_asp_net_core.Models
         protected override void OnModelCreating(ModelBuilder pModelBuilder)
         {
             base.OnModelCreating(pModelBuilder);
-            var escuela = new Escuela();
-            escuela.AñoDeCreación = 2005;
-            escuela.Id = Guid.NewGuid().ToString();
-            escuela.Nombre = "Platzi Scool";
-            escuela.Ciudad = "Bogotá";
-            escuela.Pais = "Colombia";
-            escuela.TipoEscuela = TiposEscuela.Secundaria;
-            escuela.Dirección = "Av. Calle Uno";
+
+            var escuela = CrearEscuela();
+            var cursos = CrearCursos(escuela.Id);
+            var asignaturas = CrearAsignaturas(cursos);
+            var alumnos = CrearAlumnos(cursos);
 
             pModelBuilder.Entity<Escuela>().HasData(escuela);
+            pModelBuilder.Entity<Curso>().HasData(cursos);
+            pModelBuilder.Entity<Asignatura>().HasData(asignaturas);
+            pModelBuilder.Entity<Alumno>().HasData(alumnos);
+            //Tarea: Cargar evaluaciones.
 
-            pModelBuilder.Entity<Asignatura>().HasData(
-
-                            new Asignatura { Nombre = "Matemáticas", Id = Guid.NewGuid().ToString() },
-                            new Asignatura { Nombre = "Educación Física", Id = Guid.NewGuid().ToString() },
-                            new Asignatura { Nombre = "Castellano", Id = Guid.NewGuid().ToString() },
-                            new Asignatura { Nombre = "Ciencias Naturales", Id = Guid.NewGuid().ToString() },
-                            new Asignatura { Nombre = "Programación OO", Id = Guid.NewGuid().ToString() }
-
-            );
-
-            pModelBuilder.Entity<Alumno>().HasData(CrearAlumnos());
         }
 
         #endregion
 
         #region Métodos privados
 
-        private IEnumerable<Alumno> CrearAlumnos()
+        private Escuela CrearEscuela()
+        {
+            return new Escuela()
+            {
+                AñoDeCreación = 2005,
+                Id = Guid.NewGuid().ToString(),
+                Nombre = "Platzi School",
+                Ciudad = "Bogotá",
+                Pais = "Colombia",
+                TipoEscuela = TiposEscuela.Secundaria,
+                Dirección = "Av. Calle Uno",
+            };
+        }
+
+        private IEnumerable<Alumno> CrearAlumnos(IEnumerable<Curso> pCursos)
+        {
+            var alumnos = new List<Alumno>();
+            Random random = new Random();
+
+            foreach (var curso in pCursos)
+            {
+                int cantidadAlumnos = random.Next(5, 20);
+                alumnos.AddRange(CrearAlumnos(curso.Id, cantidadAlumnos));
+            }
+
+            return alumnos;
+        }
+
+        private IEnumerable<Asignatura> CrearAsignaturas(IEnumerable<Curso> pCursos)
+        {
+            var asignaturas = new List<Asignatura>();
+            foreach (var curso in pCursos)
+                asignaturas.AddRange(CrearAsignaturas(curso.Id));
+
+            return asignaturas;
+        }
+
+        private List<Asignatura> CrearAsignaturas(string pCursoId)
+        {
+            return new List<Asignatura>() {
+                            new Asignatura {CursoId = pCursoId, Nombre = "Matemáticas", Id = Guid.NewGuid().ToString() },
+                            new Asignatura {CursoId = pCursoId, Nombre = "Educación Física", Id = Guid.NewGuid().ToString() },
+                            new Asignatura {CursoId = pCursoId, Nombre = "Castellano", Id = Guid.NewGuid().ToString() },
+                            new Asignatura {CursoId = pCursoId, Nombre = "Ciencias Naturales", Id = Guid.NewGuid().ToString() },
+                            new Asignatura {CursoId = pCursoId, Nombre = "Programación OO", Id = Guid.NewGuid().ToString()}
+            };
+        }
+
+        private IEnumerable<Alumno> CrearAlumnos(string pCursoId, int pCantidadAlumnos)
         {
             string[] nombre1 = { "José", "Josué", "Javier", "Jimena", "Jesús", "Alvaro", "Nicolás" };
             string[] apellido1 = { "Ruiz", "Sarmiento", "Uribe", "Maduro", "Trump", "Toledo", "Herrera" };
@@ -68,11 +106,34 @@ namespace platzi_asp_net_core.Models
                                from a1 in apellido1
                                select new Alumno
                                {
+                                   CursoId = pCursoId,
                                    Nombre = $"{n1} {n2} {a1}",
                                    Id = Guid.NewGuid().ToString()
                                };
 
-            return listaAlumnos.OrderBy((al) => al.Id).ToList();
+            return listaAlumnos.OrderBy((al) => al.Id).Take(pCantidadAlumnos).ToList();
+        }
+
+        private IEnumerable<Curso> CrearCursos(string pEscuelaId)
+        {
+            return new List<Curso>() {
+                CrearCurso(pEscuelaId,"101",TiposJornada.Mañana),
+                CrearCurso(pEscuelaId,"201",TiposJornada.Mañana),
+                CrearCurso(pEscuelaId,"301",TiposJornada.Mañana),
+                CrearCurso(pEscuelaId,"401",TiposJornada.Tarde),
+                CrearCurso(pEscuelaId,"501",TiposJornada.Tarde)
+            };
+        }
+
+        private Curso CrearCurso(string pEscuelaId, string pNombre, TiposJornada pTipoJornada)
+        {
+            return new Curso()
+            {
+                Id = Guid.NewGuid().ToString(),
+                EscuelaId = pEscuelaId,
+                Nombre = pNombre,
+                Jornada = pTipoJornada
+            };
         }
 
         #endregion
